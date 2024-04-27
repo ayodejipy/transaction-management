ce2a2add9b15b46cbc897790b13d0772eb827117
ayodejipy <ay.programmr@gmail.com>
2024-04-27 21:26:59 +0100

    fix: installed new all in one eslint module

--- components/TheEditor.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-	<section class="w-full mt-28 flex justify-center">#Editor-component</section>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module', '@pinia/nuxt'],
+    modules: [
+        '@nuxt/eslint',
+        '@pinia/nuxt',
+        '@nuxt/ui',
+        '@nuxtjs/color-mode',
+        '@nuxtjs/tailwindcss',
+        'nuxt-icon',
+    ],
     // eslint: {
     //   lintOnStart: false,
     // },
