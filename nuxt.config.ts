4c4d61f93401766b643d7235014e5f23b9585377
ayodejipy <ay.programmr@gmail.com>
2024-06-13 10:10:10 +0100

    fix: added preset to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    nitro: {
+        preset: 'netlify',
+    },
     runtimeConfig: {
         public: {
             apiBaseURL: process.env.NUXT_PUBLIC_API_BASE, 
